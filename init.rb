f554453fe8cf85bb2564b6f96a98bbfe77d7c50a
Eivy <modern.times.rock.and.roll+git@gmail.com>
2016-03-05 09:50:55 +0900

    Bump up version to 0.0.2

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :redmine_semiauto_done_ratio do
   name 'Redmine Progress Update plugin'
   author 'Eivy'
   description 'This is a plugin for Redmine'
-  version '0.0.1'
+  version '0.0.2'
   url 'https://github.com/Eivy/redmine_semiauto_done_ratio'
   author_url 'https://github.com/Eivy'
   require_dependency 'semiauto_done_ratio'
